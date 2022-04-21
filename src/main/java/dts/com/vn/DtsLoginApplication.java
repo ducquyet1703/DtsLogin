a8472337470a31244414c4b8e3a85005d8e872b0
ducquyet <gunnynhoc7@gmail.com>
2022-04-21 11:20:46 +0700

    Login

---
+++ src/main/java/dts/com/vn/DtsLoginApplication.java
@@ -0,0 +1,13 @@
+package dts.com.vn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtsLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtsLoginApplication.class, args);
+    }
+
+}
